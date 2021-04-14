705f38f040ba635a030e21dc00c6ce32e0abeb3d
despi <despi@MININT-5773QMD.home>
2021-04-14 21:26:44 +0200

    Simplified use of WikiPedia code

--- src/main/java/ch/zhaw/wikipedia/Main.java
+++ src/main/java/ch/zhaw/wikipedia/Main.java
@@ -184,8 +184,8 @@ public class Main {
 		Long pageId;
 		String extract;
 		for (HistoricalFigure current : sample) {
-			pageId = Main.searchPageId(current.getName());
-			extract = Main.getPage(pageId, nOfSentences);
+			pageId = WikiPediaHelper.searchPageId(current.getName());
+			extract = WikiPediaHelper.getPage(pageId, nOfSentences);
 			System.out.println(extract);
 			current.setDescription(extract);
 			n++;
@@ -195,60 +195,4 @@ public class Main {
 		System.out.println("> Found " + n + " descriptions");
 	}
 
-	private static String getPage(Long pageId, Integer sentences) {
-		String result = null;
-		String url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&pageids="
-				+ pageId.intValue() + "&exsentences=" + sentences.toString() + "&explaintext=1";
-
-		InputStream is;
-		try {
-			is = new URL(url).openStream();
-			Reader reader = new InputStreamReader(is, StandardCharsets.UTF_8);
-
-			Gson gson = new Gson();
-			Map<String, Object> deserialised = gson.fromJson(reader, Map.class);
-			Map<String, Object> query = (Map<String, Object>) deserialised.get("query");
-			Map<String, Object> pages = (Map<String, Object>) query.get("pages");
-			Map<String, Object> page = (Map<String, Object>) pages.get(pageId.toString());
-			result = (String) page.get("extract");
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
-
-	private static Long searchPageId(String name) {
-		Long pageId = null;
-		String nameUrlEncoded = null;
-		try {
-			nameUrlEncoded = URLEncoder.encode(name, StandardCharsets.UTF_8.toString());
-		} catch (UnsupportedEncodingException e1) {
-			e1.printStackTrace();
-			return pageId;
-		}
-		String url = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext=1&list=search&srsearch="
-				+ nameUrlEncoded + "&format=json";
-
-		InputStream is;
-		try {
-			is = new URL(url).openStream();
-			Reader reader = new InputStreamReader(is, StandardCharsets.UTF_8);
-
-			Gson gson = new Gson();
-			Map<String, Object> deserialised = gson.fromJson(reader, Map.class);
-			Map<String, Object> query = (Map<String, Object>) deserialised.get("query");
-			List<Object> search = (List<Object>) query.get("search");
-			if (search.size() > 0) {
-				Map<String, Object> firstHit = (Map<String, Object>) search.get(0);
-				pageId = Double.valueOf(String.valueOf(firstHit.get("pageid"))).longValue();
-			}
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return pageId;
-	}
 }
